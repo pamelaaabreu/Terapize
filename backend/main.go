e10009bcbfa33eced44f161c52447ff689d3e80c
Pamela <vitoria201547@gmail.com>
2025-04-16 13:13:54 -0300

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt" //pacote GO
+	"log"
+	"os"
+
+	_ "github.com/lib/pq" //biblioteca do POSTGRESQL
+)
+
+func main() {
+	dbURL := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Erro ao conectar no banco: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Erro ao tentar pingar o banco: %v", err)
+	}
+
+	fmt.Println("Conectado com sucesso ao PostgreSQL!")
+}
+
